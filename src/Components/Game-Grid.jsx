d8bb0933a5239b76fdcb7fec57111ca2109717dc
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-06 00:54:06 +0100

    Coverted Game-Grid to class component and created handleclick to affect state

--- src/Components/Game-Grid.jsx
+++ src/Components/Game-Grid.jsx
@@ -1,19 +1,67 @@
 import React from "react";
 import "../App.css";
-const GameGrid = (props) => {
-  return (
-    <div className="Grid-Wrapper">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
-    </div>
-  );
-};
+class GameGrid extends React.Component {
+  state = {
+    grid: ["", "", ""],
+    player1: "X",
+    player2: "O",
+    counter: 0,
+    turn: 1,
+  };
+  handleClick = (e) => {
+    let id = Number(e.target.id.slice(4));
+    console.log(typeof id);
+
+    if (e.target.innerText) return;
+
+    if (this.state.counter % 2 === 0) {
+      this.setState((currentState) => {
+        return {
+          grid: [...currentState.grid].map((gridBox, index) => {
+            if (index + 1 === id) {
+              return (gridBox = currentState.player1);
+            } else {
+              return gridBox;
+            }
+          }),
+          counter: currentState.counter + 1,
+          turn: currentState.turn + 1,
+        };
+      });
+    } else {
+      this.setState((currentState) => {
+        return {
+          grid: [...currentState.grid].map((gridBox, index) => {
+            if (index + 1 === id) {
+              return (gridBox = currentState.player2);
+            } else {
+              return gridBox;
+            }
+          }),
+          counter: currentState.counter + 1,
+          turn: currentState.turn - 1,
+        };
+      });
+    }
+  };
+  render() {
+    const { grid, counter, turn, player1, player2 } = this.state;
+    console.log(grid, counter, turn);
+
+    return (
+      <div className="Grid-Wrapper">
+        <div id="box-1" onClick={this.handleClick}></div>
+        <div id="box-2" onClick={this.handleClick}></div>
+        <div id="box-3" onClick={this.handleClick}></div>
+        <div id="box-4" onClick={this.handleClick}></div>
+        <div id="box-5" onClick={this.handleClick}></div>
+        <div id="box-6" onClick={this.handleClick}></div>
+        <div id="box-7" onClick={this.handleClick}></div>
+        <div id="box-8" onClick={this.handleClick}></div>
+        <div id="box-9" onClick={this.handleClick}></div>
+      </div>
+    );
+  }
+}
 
 export default GameGrid;
