8bdfaee3e6ca1712c5f2d3baeb9cbaf81bb1fb29
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-05 22:19:27 +0100

    Created basic html structure, created GameGrid component for game grid and adding some basic styling

---
+++ src/Components/Game-Grid.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+const GameGrid = (props) => {
+  return (
+    <div className="Grid-Wrapper">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+    </div>
+  );
+};
+
+export default GameGrid;
