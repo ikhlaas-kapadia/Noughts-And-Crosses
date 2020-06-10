bb74bcda195038565f3f3573fde60defd9071dd6
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-10 21:26:51 +0100

    Changed structure of game

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import GameGrid from "./Components/Game-Grid";
 
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Noughts and Crosses</h1>
       </header>
-      <GameGrid />
+      <section>
+        <GameGrid />
+      </section>
     </div>
   );
 }
