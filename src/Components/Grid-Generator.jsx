aed3183765b2be8b7df06b1221b3ef9e14a0d058
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-11 17:41:12 +0100

    Add dropdown menu to select grid size

--- src/Components/Grid-Generator.jsx
+++ src/Components/Grid-Generator.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const GridGenerator = (props) => {
-  const { handleGridSize } = props;
+  const { handleboardSize } = props;
   return (
     <div>
       <p>Please select your grid size</p>
-      <select onChange={handleGridSize}>
+      <select onChange={handleboardSize}>
         <option>3</option>
         <option>5</option>
         <option>7</option>
