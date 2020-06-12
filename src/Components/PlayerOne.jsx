e39c4659423b24e08099daa82603d8a77fa6823f
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-12 16:17:09 +0100

    Add score board

--- src/Components/PlayerOne.jsx
+++ src/Components/PlayerOne.jsx
@@ -2,24 +2,28 @@ import React from "react";
 
 const PlayerOne = (props) => {
   const { handleNameChange, handleIconChange, handleNameSubmit, input } = props;
-  const { name, icon } = props.playerOne;
+  const { name, icon, gamesWon } = props.player1;
 
   return (
     <div className="Player">
       <h2>
-        {!name ? "Player 1" : name}{" "}
+        {!name ? "Player 1" : name.toUpperCase()}{" "}
         <span className="Icon">{!icon ? "X" : icon}</span>
       </h2>
       <div className="NameForm">
-        <form onSubmit={handleNameSubmit} name="playerOne">
+        <form onSubmit={handleNameSubmit} name="player1">
           <label>
             <input onChange={handleNameChange} name="playerOneInput"></input>
           </label>
           <button>Change Name</button>
         </form>
-        <p>Enter Your Weapon `(letter, number, emoticon)` </p>
-        <input onChange={handleIconChange} name="playerOne"></input>
-        <div></div>
+        <p>`(Optional - Select Character)` </p>
+        <input onChange={handleIconChange} name="player1"></input>
+        <div className="Score">
+          {" "}
+          <h3>SCORE</h3>
+          {gamesWon}
+        </div>
       </div>
     </div>
   );
--- src/Components/PlayerTwo.jsx
+++ src/Components/PlayerTwo.jsx
@@ -2,24 +2,27 @@ import React from "react";
 
 const PlayerTwo = (props) => {
   const { handleNameChange, handleIconChange, handleNameSubmit, input } = props;
-  const { name, icon } = props.playerTwo;
+  const { name, icon, gamesWon } = props.player2;
 
   return (
     <div className="Player">
       <h2>
-        {!name ? "Player 2" : name}{" "}
+        {!name ? "Player 2" : name.toUpperCase()}{" "}
         <span className="Icon">{!icon ? "O" : icon}</span>
       </h2>
       <div className="NameForm">
-        <form onSubmit={handleNameSubmit} name="playerTwo">
+        <form onSubmit={handleNameSubmit} name="player2">
           <label>
             <input onChange={handleNameChange} name="playerTwoInput"></input>
           </label>
           <button>Change Name</button>
         </form>
-        <p>Enter Your Weapon `(letter, number, emoticon)` </p>
-        <input onChange={handleIconChange} name="playerTwo"></input>
-        <div></div>
+        <p>`(Optional - Select Character)` </p>
+        <input onChange={handleIconChange} name="player2"></input>
+        <div className="Score">
+          <h3>SCORE</h3>
+          {gamesWon}
+        </div>
       </div>
     </div>
   );
