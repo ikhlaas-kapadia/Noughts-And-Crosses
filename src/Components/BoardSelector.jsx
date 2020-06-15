1791d029d1d73a0682a9746f619d50853d7cbce5
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-16 00:08:13 +0100

    removed player2 component

--- src/Components/BoardSelector.jsx
+++ src/Components/BoardSelector.jsx
@@ -4,8 +4,8 @@ const BoardSelector = (props) => {
   const { handleboardSize, counter } = props;
   return (
     <div className={counter === 0 ? "Board-Selector" : "Hidden-Selector"}>
-      <p>Please select board size</p>
-      <select className="Drop-Down" onChange={handleboardSize}>
+      <label for="Drop-Down">Please select board size: </label>
+      <select className="Drop-Down" onChange={handleboardSize} name="Drop-Down">
         <option>3</option>
         <option>5</option>
         <option>7</option>
