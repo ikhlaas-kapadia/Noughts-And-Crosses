10821d779b02c31d266d621df00ee427456e250c
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-06-19 01:08:41 +0100

    Add more board sizes

--- src/Components/BoardSelector.jsx
+++ src/Components/BoardSelector.jsx
@@ -4,11 +4,14 @@ const BoardSelector = (props) => {
   const { handleboardSize, counter } = props;
   return (
     <div className={counter === 0 ? "Board-Selector" : "Hidden-Selector"}>
-      <label for="Drop-Down">Please select board size: </label>
+      <label for="Drop-Down">Select board size: </label>
       <select className="Drop-Down" onChange={handleboardSize} name="Drop-Down">
         <option>3</option>
+        <option>4</option>
         <option>5</option>
+        <option>6</option>
         <option>7</option>
+        <option>8</option>
         <option>9</option>
       </select>
     </div>
